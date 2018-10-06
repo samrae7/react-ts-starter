25d05e3b4ec9c9c6908a852cf028f4961938bccf
Sam Rae <samuelrae@gmail.com>
2018-10-06 20:19:07 +0100

    rename index.js -> index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
-class App extends React.Component {
-  render() {
+class App extends React.Component<{}, {}> {
+  render(): JSX.Element {
     return (
       <div>
         Hello world!
